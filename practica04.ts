bf2578f0969451f56a6f9c79d4a20a08a95c6d7b
Andrea <36643792+andreagbretones@users.noreply.github.com>
2019-03-18 08:16:04 +0100

    Add files via upload

---
+++ practica04.ts
@@ -0,0 +1,38 @@
+
+function inici() {
+    let inputNombre = <HTMLInputElement>document.forms["formulari"]["inputNombre"];
+    inputNombre.addEventListener("keypress",checkNombre,false);
+}
+function checkNombre(event){
+    let inputNombre =<HTMLInputElement>event.target;
+     
+    console.log(event.keyCode);
+    console.log(inputNombre.value);
+    checkNombreForm(inputNombre);
+  
+}
+function checkNombreForm(inputNombre){
+    let span = inputNombre.nextElementSibling;
+
+    if(inputNombre.value.length<3 || inputNombre.value.length>15){
+        //incorrecte
+        span.innerHTML="INCORRECTE!";
+        return false;
+    }else{
+        //correcte
+        span.innerHTML="OK";
+        return true;
+    }
+}
+function validaForm(){
+    let inputNombre = document.forms["formulari"]["inputNombre"];
+    let correcto=true;
+    
+    if(!checkNombreForm(inputNombre)){
+        correcto= false;
+    }
+    
+
+    return correcto;
+
+}
