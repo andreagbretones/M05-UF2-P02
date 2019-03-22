8d3f2d53087bdb17a5aa1247d32f1faaf1a8d3eb
sairouni <ismaelsairouni@gmail.com>
2019-03-22 12:28:15 +0100

    Cambio desarolladoress

---
+++ practica04.js
@@ -0,0 +1,32 @@
+function inici() {
+    var inputNombre = document.forms["formulari"]["inputNombre"];
+    inputNombre.addEventListener("keypress", checkNombre, false);
+}
+function checkNombre(event) {
+    var inputNombre = event.target;
+    console.log(event.keyCode);
+    console.log(inputNombre.value);
+    checkNombreForm(inputNombre);
+}
+function checkNombreForm(inputNombre) {
+    var span = inputNombre.nextElementSibling;
+    if (inputNombre.value.length < 3 || inputNombre.value.length > 15) {
+        //incorrecte
+        span.innerHTML = "INCORRECTE!";
+        return false;
+    }
+    else {
+        //correcte
+        span.innerHTML = "OK";
+        return true;
+    }
+}
+function validaForm() {
+    var inputNombre = document.forms["formulari"]["inputNombre"];
+    var correcto = true;
+    if (!checkNombreForm(inputNombre)) {
+        correcto = false;
+    }
+    return correcto;
+}
+console.log("Hola soy Isma Eso creo");
